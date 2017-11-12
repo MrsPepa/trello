8cd6c18904a0881dbf25c414bfd63de3baf6fbd8
MrsPepa <jose.leon1996@gmail.com>
2017-11-12 16:45:33 -0300

    actualizando trello

--- assets/app.js
+++ assets/app.js
@@ -1,41 +1,23 @@
-function add(){
-	//elemento padre
-	var padre =  document.getElementById('newForm');
+var original = document.getElementById('original');
+var comment = document.getElementById('comment');
+var boton = document.getElementById('btn');
 
-	//crear div contenedor
-	var newConteiner = document.createElement('div');
+comment.addEventListener('click', function(){
+	boton.classList.remove('show');
+	original.classList.add('colorO');
+})
 
-	//crear input
-	var newInput = document.createElement('input');
-
-	//crear boton
-	var boton = document.createElement('button');
-
-	//crear nodo de texto en boton
-	//var txt = document.createTextNode()
-
-	/*//crear icono
-	var plus = document.createElement('span');
-	plus.classList.add('icon-plus', 'plus');
-	plus.setAttribute('class', 'plus');
-	newConteiner.appendChild(plus);
-	*/
-
-	//agregar las clases
-	newConteiner.setAttribute('id','contenedor');
-	newInput.setAttribute('id','inputCreate');
-	boton.setAttribute('id', 'btn');
-
-	//agregar al padre
-	newConteiner.appendChild(newInput);
-	newConteiner.appendChild(boton);
-	padre.appendChild(newConteiner);
-
-/*
-	//var clase input
-	var keep = document.getElementsByClassName('inputOriginal');
-
-	keep.addEventListener('click', function(){
-		return inputOriginal;
-	})*/
-}
+boton.addEventListener('click', function(){
+	//guardar lo que usuario escribio
+	comment.value;
+	//limpiar input
+	comment.value = '';
+	//sacar boton
+	boton.classList.remove('show');
+	//creamos añadir tarea
+	var addTarea = document.createElement('p');
+	var txtArea = document.createTextNode('Añadir una tarea');
+	//añadimos al div
+	addTarea.appendChild(txtArea);
+	original.appendChild(addTarea);
+})
